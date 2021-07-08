2d0e4568f553e5099e5ddcbc2b0b279a2b4d369c
Elder Pereira <elder-guedes@hotmail.com>
2021-07-08 19:15:32 -0300

    Foram adicionados as funções de soma e subtração da calculadora

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def soma(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def main():
+    x=10
+    y=20
+    resultado1 = soma(x,y)
+    resultado2 = sub(x,y)
+    print(f'Resultados {resultado1} , {resultado2}')
+    
+main()
