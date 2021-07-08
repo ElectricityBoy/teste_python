cfc86305168f9ebafadb65854f6779a60e4e9f78
Elder Pereira <elder-guedes@hotmail.com>
2021-07-08 19:26:56 -0300

    Implementando novas funções

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,19 @@
 def soma(x,y):
     return x+y
+    
 def sub(x,y):
     return x-y
+
 def main():
     x=10
     y=20
     resultado1 = soma(x,y)
     resultado2 = sub(x,y)
-    print(f'Resultados {resultado1} , {resultado2}')
+    resultado3 = mult(x,y)
+
+    print(f'Resultados {resultado1} , {resultado2}, {resultado3}')
+
+def mult(x,y):
+    return x*y
     
 main()
